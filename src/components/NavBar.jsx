9713f634bc226f73b5e2381239e990b3066b16de
DevChris38 <christopher.arthaud@gmail.com>
2023-10-11 15:32:52 +0200

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,9 @@ export default function NavBar({pokemonList, setPokemonIndex}) {
 
     return(
         <>
-            <ul>
                 {pokemonList.map((pokemon, index)=> (
-                    <li key={pokemon.name}>
-                        <button onClick={()=>setPokemonIndex(index)}>{pokemon.name}</button>
-                    </li>
+                        <button key={pokemon.name} onClick={()=>setPokemonIndex(index)}>{pokemon.name}</button>
                 ))}
-            </ul>
         </>
     )
 
